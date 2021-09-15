cc90c179b74b500712277937c200a2345121a076
pragyeshjain <pragyeshjn@gmail.com>
2021-09-15 15:41:00 +0530

    First commit

---
+++ GitHubReposJavaProject/src/com/lti/demo/UserApp.java
@@ -0,0 +1,12 @@
+package com.lti.demo;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		Product p=new Product(101,"mobile",5000);
+		System.out.println(p);
+		System.out.println("hggjh");
+
+	}
+
+}
